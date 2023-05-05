e6d11065a9e3adaab0118a387e64a34b34578256
cosminabarbu <cosmina36341@gmail.com>
2023-05-05 23:22:24 +0300

    lab9

--- index.js
+++ index.js
@@ -2,17 +2,109 @@ const express = require("express");
 const fs = require('fs');
 const path = require("path");
 const sharp = require('sharp');
+const sass=require('sass');
+const {Client}=require('pg');
+
+var client= new Client({database:"bdshop",
+        user:"cosmina",
+        password:"parola",
+        host:"localhost",
+        port:5432});
+client.connect();
+
+
+client.query("select * from lab8_12", function(err, rez){
+    console.log("Eroare BD",err);
+ 
+    console.log("Rezultat BD",rez.rows);
+});
 
 obGlobal={
     obErori:null,
-    obImagini:null
+    obImagini:null,
+    folderScss: path.join(__dirname, "resurse/scss"),
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup"),
+    optiuniMeniu:[]
 }
+
+client.query("select * from unnest(enum_range(null::tipuri_produse))", function(err, rezCateg){
+    if(err){
+        console.log(err);
+    }
+    else{
+        obGlobal.optiuniMeniu = rezCateg.rows;
+    }
+});
+
+obGlobal.optiuniMeniu
+
+
 
 app= express();
 
 console.log("Folder proiect: ", __dirname);
 console.log("Cale fisier: ", __filename);
 console.log("Director de lucru: ", process.cwd());
+
+app.get("/produse",function(req, res){
+
+
+    //TO DO query pentru a selecta toate produsele
+    //TO DO se adauaga filtrarea dupa tipul produsului
+    //TO DO se selecteaza si toate valorile din enum-ul categ_prajitura
+
+    client.query("select * from unnest(enum_range(null::categ_prajitura))", function(err,rezCateg){
+        if(err){
+            console.log(err);
+            afiseazaEroare(res, 2);
+        }
+        else {
+            let conditieWhere = "";
+            if(req.query.tip)    
+                conditieWhere= ` where tip_produs='${req.query.tip}' `;  //"where tip='"+req.query.tip+"'" 
+    
+            client.query("select * from prajituri"+conditieWhere , function( err, rez){
+                //console.log(300)
+                if(err){
+                    console.log(err);
+                    afiseazaEroare(res, 2);
+                }
+                else{
+                    console.log(rez);
+                    res.render("pagini/produse", {produse:rez.rows, optiuni:rezCateg.rows});
+
+                }
+                    
+            });
+        }
+    });
+
+
+
+
+});
+
+
+app.get("/produs/:id",function(req, res){
+    console.log(req.params);
+   
+    client.query(`select * from prajituri where id=${req.params.id}`, function( err, rezultat){
+        if(err){
+            console.log(err);
+            afiseazaEroare(res, 2);
+        }
+        else
+            res.render("pagini/produs", {prod:rezultat.rows[0]});
+    });
+});
+
+// client.query("select * from unnest(enum_range(null::categ_prajitura))",function(err, rez){
+//     console.log(err);
+//     console.log(rez);
+// })
+
+
 
 app.set("view engine", "ejs");
 //motor de template
@@ -21,6 +113,12 @@ app.set("view engine", "ejs");
 app.use("/resurse", express.static(__dirname + "/resurse"));
 //express.static e o functie care returneaza un obiect
 //asa "livrez" resursele pentru site 
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
+
+app.use("/*",function(req, res, next){
+    res.locals.optiuniMeniu=obGlobal.optiuniMeniu;
+    next();
+});
 
 app.use(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function (req, res) {
     afiseazaEroare(res, 403);
@@ -45,18 +143,71 @@ app.get("/promotii", function(req, res){
 })
 
 
+
+
 app.get("/*.ejs",function(req, res){
     afiseazaEroare(res, 400);
 })
 
-vectorFoldere = ["temp", "temp1"]
+vectorFoldere = ["temp", "temp1", "backup"]
 
 for(let folder of vectorFoldere){
+    //let cale_folder = __dirname + "/" + folder;
     let cale_folder = path.join(__dirname, folder);
+    //console.log(cale_folder);
     if(!fs.existsSync(cale_folder))
     fs.mkdirSync(cale_folder);
 } //creeaza folderele daca nu exista deja
 
+
+
+function compileazaCss(caleScss, caleCss){
+    if(!caleCss) {
+    //let vectorCale=caleScss.split("\\")
+    //let numeFisExt=vectorCale[vectorCale.length-1] //nume fisier cu extensie
+    let numeFisExt = path.basename(caleScss);
+    let numeFis = numeFisExt.split(".")[0] ///"a.scss" -> ["a", "scss"]
+    caleCss = numeFis + ".css"; 
+   }
+    
+   if (!path.isAbsolute(caleScss))
+      caleScss=path.join(obGlobal.folderScss, caleScss)
+   if (!path.isAbsolute(caleCss))
+      caleCss=path.join(obGlobal.folderCss, caleCss)
+    ///la acest punct avem cai absolute in caleScss si caleCss
+
+    console.log("cale:", caleCss);
+    
+    let vectorCale=caleCss.split("\\");
+    let numeFisCss=vectorCale[vectorCale.length-1];
+    if(fs.existsSync(caleCss)){
+        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup,numeFisCss)) //+(new Date()).getTime()
+    }
+
+    rez=sass.compile(caleScss, {"sourceMap":true});
+    fs.writeFileSync(caleCss, rez.css)
+    console.log("compilare scss", rez);
+}
+
+//compileazaScss("a.scss");
+
+vFisiere=fs.readdirSync(obGlobal.folderScss); //da vector de string uri cu numele fisierelor
+    for(let numeFis of vFisiere){
+        if(path.extname(numeFis) == ".scss"){
+            compileazaCss(numeFis);
+        }
+    }
+
+
+fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
+    //console.log(eveniment, numeFis);
+    if(eveniment == "change" || eveniment == "rename"){
+        let caleCompleta = path.join(obGlobal.folderScss, numeFis);
+        if(fs.existsSync(caleCompleta)){
+            compileazaCss(caleCompleta);
+        }
+    }
+})
 
 app.get("/*", function(req, res){
     try{
